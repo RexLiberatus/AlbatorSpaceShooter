3b51a7190b56523087add28c29d78812642a9418
Rex <44822317+RexLiberatus@users.noreply.github.com>
2019-08-06 16:52:54 +0200

    update Firing
    fire with pulling bullets up and down

---
+++ Assets/Scripts/AmmoBehaviour.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmmoBehaviour : MonoBehaviour
+{
+    private void Start()
+    {
+        
+    }
+    
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("aïe");
+        gameObject.SetActive(false);
+        gameObject.GetComponent<Rigidbody2D>().velocity=Vector2.zero;
+        gameObject.transform.position = Vector3.zero;
+    }
+}
--- Assets/Scripts/Firing.cs
+++ Assets/Scripts/Firing.cs
@@ -4,28 +4,87 @@ using UnityEngine;
 
 public class Firing : MonoBehaviour
 {
+    #region data
     [SerializeField]
     private GameObject prefabAmmo;
 
-    public GameObject PrefabAmmo { get => prefabAmmo; set => prefabAmmo = value; }
+    [SerializeField]
+    private float speed;
 
+    [SerializeField]
     private List<GameObject> ammoReserve;
 
+    #endregion
+    #region accessors
+    private bool trigger = false;
+    public GameObject PrefabAmmo { get => prefabAmmo; set => prefabAmmo = value; }
+    public float Speed { get => speed; set => speed = value; }
+    #endregion
+
     // Start is called before the first frame update
-    void Awaket()
+    void Awake()
     {
+
+        ammoReserve = new List<GameObject>();
         for (int i = 0; i < 6; i++)
         {
-            if (prefabAmmo !=null)
-            {
-
-            }
-            
+            ammoReserve.Add(Instantiate(prefabAmmo, transform.position, Quaternion.identity));
+        }
+    }
+    private void Start()
+    {
+        foreach (var item in ammoReserve)
+        {
+            item.SetActive(false);
         }
     }
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            trigger = false;
+        }
+        else if (Input.GetKeyDown(KeyCode.S))
+        {
+            trigger = true;
+        }
+    }
+    public void Shoot()
+    {
+        Debug.Log("i shoot");
+        GameObject ammo = GiveMeTheNextAvailableAmmo();
+       ApplyDownOrUpForce(trigger, ammo);
+
+
+        //Debug.Log("i1");
+        //ammoReserve[i].SetActive(true);
+
+    }
+    private void ApplyDownOrUpForce(bool isDown, GameObject ammo)
+    {
+        ammo.transform.position = gameObject.transform.position;
+        ammo.SetActive(true);
+        Debug.Log("Ammo => ", ammo);
+        ammo.GetComponent<Rigidbody2D>().AddForce( isDown ? Vector2.down: Vector2.up * speed, ForceMode2D.Impulse);
+    }
+
+    private GameObject GiveMeTheNextAvailableAmmo()
+    {
         
+        for (int i = 0; i < ammoReserve.Count; i++)
+        {
+            if (ammoReserve[i].activeSelf == false)
+              return    ammoReserve[i]; 
+
+
+        }
+
+        return null;
+    }
+
+    private bool checkICanFire(bool hasAlreadyShot, int i)
+    {
+        return ammoReserve[i].activeSelf == false && hasAlreadyShot == true;
     }
 }
