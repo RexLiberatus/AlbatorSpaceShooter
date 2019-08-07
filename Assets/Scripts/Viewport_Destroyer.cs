115a7482330457afb004e3654f32b9973542270f
Rex <RexLiberatus@users.noreply.github.com>
2019-08-07 16:17:59 +0200

    destroy mobs when out of left cam range

---
+++ Assets/Scripts/Viewport_Destroyer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Viewport_Destroyer : MonoBehaviour
+{
+
+    public Transform m_target;
+    public Vector2 m_viewport;
+
+
+    void Update()
+    {
+        CheckDisablehPosition();
+    }
+
+    private void CheckDisablehPosition()
+    {
+  
+        m_target = transform;
+        m_viewport = Camera.main.WorldToViewportPoint(m_target.position);
+        if (m_viewport.x<=-1)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
