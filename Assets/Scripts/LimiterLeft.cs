6eb8c9492b43c4f2aaea27a37e146b0a5010e53c
Rex <44822317+RexLiberatus@users.noreply.github.com>
2019-08-07 14:18:31 +0200

    Update: PlayerBehaviour & UiValues

---
+++ Assets/Scripts/LimiterLeft.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LimiterLeft : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        DeleteMobs(collision);
+        BlockPlayer(collision);
+    }
+
+    private static void BlockPlayer(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            collision.gameObject.transform.position = collision.gameObject.transform.position;
+        }
+    }
+
+    private static void DeleteMobs(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Mob")
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+}
